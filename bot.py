f7e7deb3b6a2f6509ded50173177ec0395802b74
KarlClarence <163532381+KarlClarence@users.noreply.github.com>
2024-09-18 17:34:53 +0800

    更新 bot.py

--- bot.py
+++ bot.py
@@ -29,10 +29,10 @@ openai.api_base = config["openai"].get("api_url")
 model_name = config["openai"].get("model", "gpt-35-turbo")
 
 client = AzureOpenAI(
-  azure_endpoint = "https://mika-1.openai.azure.com/openai/deployments/mika4/chat/completions?api-
+  	azure_endpoint = "https://mika-1.openai.azure.com/openai/deployments/mika4/chat/completions?api-
 version=2024-02-15-preview",
-  api_key="0825bf94904a4801b548b2069d63327a",
-  api_version="2024-02-15-preview"
+  	api_key="0825bf94904a4801b548b2069d63327a",
+  	api_version="2024-02-15-preview"
 )
 
 def countToken():
